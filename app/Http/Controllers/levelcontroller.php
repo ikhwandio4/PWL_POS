87e83e260e3803123a4d218d0d6490d2bd64b612
ikhwandio4 <basroni12062018@gmail.com>
2024-03-03 14:40:36 +0700

    praktikum 4 membuat DB Facade

---
+++ app/Http/Controllers/levelcontroller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class levelcontroller extends Controller
+{
+    public function index()
+    {
+        // DB::insert ('insert into m_levels(level_kode, level_nama,created_at) values (?,?,?)',['cus','pelanggan',now()]);
+        // return 'insert data baru berhasil ';
+
+        // $row = DB::update('update m_levels set level_nama = ? where level_kode = ? ', ['Customer','CUS']);
+        // return 'update berhasil . jumlah data yang diupdate: ' .$row.'baris';
+
+        // $row = DB::delete ('delete from m_levels where level_kode = ?',['CUS']);
+        // return 'Delete berhasil .Jumlah data yang dihapus: '. $row.'baris';
+
+        $data = DB::select('select * from m_levels');
+        return view('level',['data' => $data]);
+    }
+}
